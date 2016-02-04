33ff3364df0e3f233e73f32543ed80562570de72
IndieForger <indieforger@gmail.com>
2016-02-04 02:05:27 +0000

    getLastUpdateDate()

--- myhead-processor.js
+++ myhead-processor.js
@@ -2,18 +2,27 @@
 
 const
   exec = require('child_process').exec,
-  fs = require('fs')
+  fs = require('fs'),
+  internal = {},
+  processor = {}
 
-module.exports = {
-  getCreationDate: getCreationDate
+module.exports = processor
+
+processor.getCreationDate = function(filename) {
+  // source: http://stackoverflow.com/questions/2390199/finding-the-date-time-a-file-was-first-added-to-a-git-repository
+  let bashCmd = `git log --diff-filter=A --follow --format=%aD -1 -- ${filename}`
+  return internal.runBashCommand(bashCmd, filename)
 }
 
-function getCreationDate(filename) {
-  return new Promise((resolve, reject) => {
-    // source: http://stackoverflow.com/questions/2390199/finding-the-date-time-a-file-was-first-added-to-a-git-repository
-    let bashCmd = `git log --diff-filter=A --follow --format=%aD -1 -- ${filename}`
-    let exists = false
+processor.getLastUpdateDate = function(filename) {
+  // grep list of authors from the file log
+  let bashCmd = `git log -1 --format=%cd -- ${filename}`
+  return internal.runBashCommand(bashCmd, filename)
+}
 
+internal.runBashCommand = function(bashCmd, filename) {
+  let exists = false
+  return new Promise((resolve, reject) => {
     fs.stat(filename, (err, stat) => {
       if ((err !== null)) return reject(err)
       exec(bashCmd, (error, stdout, stderr) => {
@@ -21,8 +30,6 @@ function getCreationDate(filename) {
         if (error !== null) return reject(error)
         resolve(stdout)
       })
-
     })
-
   })
 }
--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ const
   should = require('should'),
   processor = require('../'),
   path = require('path'),
-  mockFilePath = path.resolve(__dirname, 'mock-article.md'),
-  creationDate = 'Wed, 3 Feb 2016 20:37:09 +0000'
+  mockFilePath = path.resolve(__dirname, 'mock-article.md')
 
 describe('#getCreationDate()', function () {
   it('should return date', function (done) {
+    let creationDate = 'Wed, 3 Feb 2016 20:37:09 +0000'
     processor
       .getCreationDate(mockFilePath)
       .then((stdout) => {
@@ -23,6 +23,33 @@ describe('#getCreationDate()', function () {
   it('should report error', function (done) {
     processor
       .getCreationDate('broken-file-name')
+      .then((stdout) => {
+          done(new Error('Unexpected Error'))
+      }).catch((error) => { try {
+        error.message.should.startWith('ENOENT: no such file or directory')
+        done()
+      } catch (err) { done(err) }})
+  })
+})
+
+describe('#getLastUpdateDate()', function () {
+  it('should return date', function (done) {
+    let creationDate = 'Wed Feb 3 20:40:33 2016 +0000'
+    processor
+      .getLastUpdateDate(mockFilePath)
+      .then((stdout) => {
+        let testTime = (new Date(creationDate)).getTime()
+        let expTime = (new Date(stdout)).getTime()
+        try {
+          testTime.should.be.exactly(expTime)
+          done()
+        } catch (err) { done(err) }
+      }).catch((error) => done(error))
+  })
+
+  it('should report error', function (done) {
+    processor
+      .getLastUpdateDate('broken-file-name')
       .then((stdout) => {
           done(new Error('Unexpected Error'))
       }).catch((error) => { try {
